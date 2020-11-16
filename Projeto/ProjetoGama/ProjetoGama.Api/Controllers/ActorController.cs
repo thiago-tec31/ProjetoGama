9440aa38bbadb822d4357a71ae714818fb3651bd
Thiago Santos <39251786+thiago-tec31@users.noreply.github.com>
2020-11-16 00:27:21 -0300

    6
    6

--- Projeto/ProjetoGama/ProjetoGama.Api/Controllers/ActorController.cs
+++ Projeto/ProjetoGama/ProjetoGama.Api/Controllers/ActorController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Marraia.Notifications.Base;
 using Marraia.Notifications.Models;
 using MediatR;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoGama.Application.ProjetoGama.Input;
